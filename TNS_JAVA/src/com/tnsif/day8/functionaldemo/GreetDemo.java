22b00ff5cd38f6aa26a0ee5e59b3e205e5f33010
Bhakti Raut <rautbhakti090@gmail.com>
2025-07-13 23:56:49 +0530

    Functional demo

---
+++ TNS_JAVA/src/com/tnsif/day8/functionaldemo/GreetDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.day8.functionaldemo;
+
+
+public class GreetDemo implements GreetInterface {
+
+	@Override
+	public void greet() {
+		System.out.println("Good morning ----");
+		
+	}
+
+	
+}
---
+++ TNS_JAVA/src/com/tnsif/day8/functionaldemo/GreetInterface.java
@@ -0,0 +1,8 @@
+package com.tnsif.day8.functionaldemo;
+
+@FunctionalInterface
+public interface GreetInterface {
+
+	void greet();
+	
+}
