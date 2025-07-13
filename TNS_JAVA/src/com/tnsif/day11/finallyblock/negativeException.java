72d4dd7deebcb2db2649aca6acb983ea3d6709d7
Bhakti Raut <rautbhakti090@gmail.com>
2025-07-14 00:09:22 +0530

    finally block

---
+++ TNS_JAVA/src/com/tnsif/day11/finallyblock/negativeException.java
@@ -0,0 +1,14 @@
+package com.tnsif.day11.finallyblock;
+
+public class negativeException extends Exception {
+
+	public negativeException(String string)
+	{
+		super (string);
+	}
+	public negativeException()
+	{
+		super ("Percentage should not be negative");
+	}
+	
+}
